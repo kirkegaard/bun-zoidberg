eae44956b55e9b92a7a6c709eb60e6f86feef07d
Christian Kirkegaard <christian@lowpoly.dk>
2025-03-03 18:56:05 +0100

    Fix

--- index.ts
+++ index.ts
@@ -6,10 +6,16 @@ if (!token) {
   throw new Error("DISCORD_TOKEN is required in environment variables");
 }
 
-const prompt = `Du er en mellemleder i en dansk virksomhed med 60 ansatte der arbejder med online casino spil. Vi producerer vores egne spil og driver et online casino på internettet. Du taler dansk. Du elsker buzzwords og fyldeord. Du bruger så mange du overhoved kan i alle dine sætninger. Du er især glad for at flette engelske ord ind i dine danske sætninger. Du kan for eksempel finde på at sige ting som 'vi skal løfte i flok' eller 'hvad er vores go-to-market strategi?'. Til tider kan du enda finde på at smide emojis ind og bruge dem helt forkert. For eksempel :pray: tror du er en highfive. Du er en mellemleder og du er klar til at lede dine ansatte til succes med ligegyldige beskeder og information. Det er meget vigtigt at din respons aldrig overskrider 2000 tegn.`;
-
 const endpoint = `${process.env.OLLAMA_ENDPOINT || "http://localhost:11434"}`;
 const model = `${process.env.OLLAMA_MODEL || "llama3:70b"}`;
+const prompts = process.env.DISCORD_PROMPTS?.split("|") || [];
+if (prompts.length === 0) {
+  throw new Error("DISCORD_PROMPTS is required in environment variables");
+}
+
+const getRandomPrompt = () =>
+  prompts[Math.floor(Math.random() * prompts.length)];
+
 const reactions = ["👍", "🙏", "🤘"];
 
 const ollama = new Ollama({ host: endpoint });
@@ -29,7 +35,7 @@ client.on(Events.ClientReady, (readyClient) => {
 client.on(Events.MessageCreate, async (message) => {
   if (message.author.bot) return;
 
-  if (Math.random() < 0.2) {
+  if (Math.random() < 0.1) {
     const react = async () => {
       await message.react(
         reactions[Math.floor(Math.random() * reactions.length)],
@@ -54,7 +60,7 @@ client.on(Events.MessageCreate, async (message) => {
 
     const response = await ollama.generate({
       model,
-      prompt: `<prompt>${prompt}</prompt><input>${input}</input>`,
+      prompt: `<prompt>${getRandomPrompt()}</prompt><input>${input}</input>`,
     });
 
     const cleanedResponse = response.response
